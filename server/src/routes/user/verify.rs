fada36b5eb6797b927aadcb1f7aa52c4eb6b9558
neursh <31346263+neursh@users.noreply.github.com>
2025-11-14 09:28:19 +0700

    Verify email returrn token for now, will fix to set cookie

--- server/src/routes/user/verify.rs
+++ server/src/routes/user/verify.rs
@@ -1,22 +1,35 @@
 use axum::{ Json, extract::State };
 use reqwest::StatusCode;
+use serde_json::json;
 use serde::Deserialize;
 
-use crate::{ base::{ self, response::ResponseModel }, routes::user::RouteState };
+use crate::{
+    base::{ self, response::ResponseModel },
+    routes::user::RouteState,
+    utils::jwt::UserClaims,
+};
 
 #[derive(Deserialize)]
 pub struct VerifyPayload {
-    pub token: String,
+    pub verify_code: String,
 }
 
 pub async fn handler(
     State(state): State<RouteState>,
     Json(payload): Json<VerifyPayload>
 ) -> (StatusCode, Json<ResponseModel>) {
-    match state.koii_database.users.verify(payload.token).await {
+    match state.koii_database.users.verify(payload.verify_code).await {
         Ok(done) => {
-            if done {
-                return base::response::success(StatusCode::OK);
+            if let Some(_id) = done {
+                let token = state.jwt.generate(UserClaims {
+                    _id,
+                    exp: jsonwebtoken::get_current_timestamp() + 60 * 15,
+                });
+
+                return match token {
+                    Ok(token) => base::response::result(StatusCode::OK, json!({ "token": token })),
+                    Err(_) => base::response::internal_error(),
+                };
             } else {
                 return base::response::error(
                     StatusCode::NOT_FOUND,
