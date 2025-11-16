f4e971d77b8fce5ee569f7e4d0fd42d39b715c3f
neursh <31346263+neursh@users.noreply.github.com>
2025-11-16 19:45:26 +0700

    Clean up stuff

--- server/src/routes/user/verify.rs
+++ server/src/routes/user/verify.rs
@@ -1,8 +1,12 @@
-use axum::{ Json, extract::State };
+use axum::{ Extension, Json, extract::State };
 use reqwest::StatusCode;
 use serde::Deserialize;
 
-use crate::{ base::{ self, response::ResponseModel }, routes::user::RouteState };
+use crate::{
+    base::{ self, response::ResponseModel },
+    routes::user::RouteState,
+    utils::middlewares::{ AuthorizationInfo, AuthorizationStatus },
+};
 
 #[derive(Deserialize)]
 pub struct VerifyPayload {
@@ -10,28 +14,37 @@ pub struct VerifyPayload {
 }
 
 pub async fn handler(
+    Extension(authorization_info): Extension<AuthorizationInfo>,
     State(state): State<RouteState>,
     Json(payload): Json<VerifyPayload>
 ) -> ResponseModel {
-    match state.koii_database.users.verify(payload.verify_code).await {
+    if let AuthorizationStatus::Authorized = authorization_info.status {
+        return base::response::error(
+            StatusCode::FORBIDDEN,
+            "There's already an active user.",
+            None
+        );
+    }
+
+    return match state.koii_database.users.verify(payload.verify_code).await {
         Ok(done) => {
-            if let Some(id) = done {
-                return match base::session::create(&state.jwt, id) {
-                    Ok(session_cookie) =>
-                        base::response::success(StatusCode::OK, Some(session_cookie)),
-                    Err(_) => base::response::internal_error(None),
-                };
-            } else {
-                return base::response::error(
-                    StatusCode::NOT_FOUND,
-                    "There's no account associated to this verify token.",
-                    None
-                );
+            match done {
+                Some(id) =>
+                    match base::session::create(&state.koii_database.refresh, &state.jwt, id).await {
+                        Ok(headers) => base::response::success(StatusCode::OK, Some(headers)),
+                        Err(_) => base::response::internal_error(None),
+                    }
+                None =>
+                    base::response::error(
+                        StatusCode::NOT_FOUND,
+                        "There's no account associated to this verify token.",
+                        None
+                    ),
             }
         }
-        Err(s) => {
-            println!("{:?}", s);
-            return base::response::internal_error(None);
+        Err(error) => {
+            println!("{:?}", error);
+            base::response::internal_error(None)
         }
-    }
+    };
 }
