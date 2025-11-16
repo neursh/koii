6c5b819b2609672d0203074cc4b7bf858d87cd41
neursh <31346263+neursh@users.noreply.github.com>
2025-11-16 19:45:41 +0700

    Simple delete

---
+++ server/src/routes/user/delete.rs
@@ -0,0 +1,69 @@
+use axum::{ Extension, extract::State, response::AppendHeaders };
+use reqwest::{ StatusCode, header::SET_COOKIE };
+
+use crate::{
+    base::{ self, response::ResponseModel },
+    routes::user::RouteState,
+    utils::middlewares::{ AuthorizationInfo, AuthorizationStatus },
+};
+
+pub async fn handler(
+    Extension(authorization_info): Extension<AuthorizationInfo>,
+    State(state): State<RouteState>
+) -> ResponseModel {
+    match authorization_info.status {
+        AuthorizationStatus::Authorized => (),
+        _ => {
+            return base::response::error(StatusCode::UNAUTHORIZED, "Get out.", None);
+        }
+    }
+
+    let token = authorization_info.token.unwrap();
+
+    let result = match state.koii_database.users.delete(token.id).await {
+        Ok(result) => result,
+        Err(error) => {
+            eprintln!("{}", error);
+            return base::response::internal_error(None);
+        }
+    };
+
+    if result {
+        return base::response::success(
+            StatusCode::OK,
+            Some(
+                AppendHeaders(
+                    vec![
+                        (
+                            SET_COOKIE,
+                            "token=; HttpOnly; SameSite=Lax; Secure; Path=/; Domain=.koii.space; Max-Age=0".to_string(),
+                        ),
+                        (
+                            SET_COOKIE,
+                            "refresh=; HttpOnly; SameSite=Lax; Secure; Path=/; Domain=.koii.space; Max-Age=0".to_string(),
+                        )
+                    ]
+                )
+            )
+        );
+    }
+
+    base::response::error(
+        StatusCode::CONFLICT,
+        "The user is already deleted. Why is the cookie still here?",
+        Some(
+            AppendHeaders(
+                vec![
+                    (
+                        SET_COOKIE,
+                        "token=; HttpOnly; SameSite=Lax; Secure; Path=/; Domain=.koii.space; Max-Age=0".to_string(),
+                    ),
+                    (
+                        SET_COOKIE,
+                        "refresh=; HttpOnly; SameSite=Lax; Secure; Path=/; Domain=.koii.space; Max-Age=0".to_string(),
+                    )
+                ]
+            )
+        )
+    )
+}
