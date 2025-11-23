8cb472a8619d2a90d4b1ee9b3a8f9fc02e1e40ce
neursh <31346263+neursh@users.noreply.github.com>
2025-11-23 07:59:59 +0700

    Router a bit more helpful

--- client/src/hooks/router.tsx
+++ client/src/hooks/router.tsx
@@ -1,5 +1,24 @@
 import { hookstate, useHookstate } from 'hookstate';
-import { useLayoutEffect, useRef, useState } from 'react';
+import { useLayoutEffect, useRef, useState, type ReactElement } from 'react';
+import { URLPattern } from 'urlpattern-polyfill/urlpattern';
+
+export class Router {
+  destinations: [URLPattern, ReactElement][];
+
+  constructor(load: { [key: string]: ReactElement }) {
+    this.destinations = Object.entries(load).map((destination) => [
+      new URLPattern(destination[0], window.location.origin),
+      destination[1],
+    ]);
+  }
+
+  match(location: string) {
+    const destination = this.destinations.find((destination) =>
+      destination[0].test(location)
+    );
+    return destination ? destination[1] : null;
+  }
+}
 
 const currentHrefGlobal = hookstate(window.location.href);
 
@@ -11,18 +30,13 @@ window.addEventListener('popstate', () => {
 export function useRoute(delay?: number) {
   const currentHref = useHookstate(currentHrefGlobal);
   const [route, setRoute] = useState(new URL(currentHrefGlobal.value));
-  const timeoutHolder = useRef(setTimeout(() => {}, 0));
+  const timeoutHolder = useRef(setTimeout(() => {}));
 
   useLayoutEffect(() => {
-    if (delay) {
-      clearTimeout(timeoutHolder.current);
-      timeoutHolder.current = setTimeout(() => {
-        setRoute(new URL(currentHref.value));
-      }, delay);
-    } else {
-      // eslint-disable-next-line react-hooks/set-state-in-effect
+    clearTimeout(timeoutHolder.current);
+    timeoutHolder.current = setTimeout(() => {
       setRoute(new URL(currentHref.value));
-    }
+    }, delay);
 
     return () => {
       clearTimeout(timeoutHolder.current);
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,37 +1,20 @@
-import { StrictMode, useMemo, type ReactElement } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { URLPattern } from 'urlpattern-polyfill/urlpattern';
-import { useRoute } from './hooks/router.tsx';
+import { Router, useRoute } from './hooks/router.tsx';
 import './index.css';
 import Layout from './Layout/index.tsx';
 
-const destinations: { [key: string]: ReactElement } = {
-  '/': <></>,
+const destinations = new Router({
   '/apps': <p>Welcome to apps</p>,
-};
+});
 
 export function Container() {
   const route = useRoute(500);
-  const destinationsParse: [URLPattern, ReactElement][] = useMemo(
-    () =>
-      Object.entries(destinations).map((destination) => [
-        new URLPattern(destination[0], window.location.origin),
-        destination[1],
-      ]),
-    []
-  );
-
-  const outlet = useMemo(() => {
-    const destination = destinationsParse.find((destination) =>
-      destination[0].test(route.href)
-    );
-    return destination ? destination[1] : null;
-  }, [destinationsParse, route.href]);
 
   return (
     <main>
       <Layout />
-      {outlet}
+      {destinations.match(route.href)}
     </main>
   );
 }
