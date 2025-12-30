0c5432d12ecfa15cbdad40e1918809dbaa3447ae
neursh <31346263+neursh@users.noreply.github.com>
2025-12-30 10:36:57 +0700

    Apply verify cache to the endpoint

--- server/src/routes/user/verify.rs
+++ server/src/routes/user/verify.rs
@@ -2,7 +2,10 @@ use axum::{ Extension, Json, extract::State, http::StatusCode };
 use serde::Deserialize;
 
 use crate::{
-    base::{ self, response::ResponseModel }, middlewares::auth::{ AuthorizationInfo, AuthorizationStatus }, routes::user::UserRoutesState, utils
+    base::{ self, response::ResponseModel },
+    middlewares::auth::{ AuthorizationInfo, AuthorizationStatus },
+    routes::user::UserRoutesState,
+    utils,
 };
 
 #[derive(Deserialize)]
@@ -23,29 +26,47 @@ pub async fn handler(
         );
     }
 
-    return match state.app.store.users.verify(payload.verify_code).await {
-        Ok(Some(id)) => {
-            match
-                utils::session::create(
-                    &mut state.app.cache.refresh.clone(),
-                    &state.app.jwt,
-                    id
-                ).await
-            {
-                Ok(headers) => base::response::success(StatusCode::OK, Some(headers)),
-                Err(_) => base::response::internal_error(None),
-            }
-        }
+    // Get user id from cache.
+    let user_id = match state.app.cache.verify.clone().permit(&payload.verify_code).await {
+        Ok(Some(user_id)) => user_id,
         Ok(None) => {
-            base::response::error(
+            return base::response::error(
                 StatusCode::NOT_FOUND,
                 "There's no account associated to this verify token.",
                 None
-            )
+            );
         }
         Err(error) => {
             println!("{:?}", error);
-            base::response::internal_error(None)
+            return base::response::internal_error(None);
+        }
+    };
+
+    // Confirm with database.
+    match state.app.store.users.confirm(&user_id).await {
+        Ok(true) => {}
+        Ok(false) => {
+            return base::response::error(
+                StatusCode::NOT_FOUND,
+                "There's no account associated to this verify token.",
+                None
+            );
+        }
+        Err(error) => {
+            println!("{:?}", error);
+            return base::response::internal_error(None);
+        }
+    }
+
+    // Return session.
+    match
+        utils::session::create(&mut state.app.cache.refresh.clone(), &state.app.jwt, user_id).await
+    {
+        Ok(headers) => {
+            return base::response::success(StatusCode::OK, Some(headers));
+        }
+        Err(_) => {
+            return base::response::internal_error(None);
         }
     };
 }
