6f414995288649654247aac8c48bcc36bc5d6a91
neursh <31346263+neursh@users.noreply.github.com>
2025-12-30 10:34:18 +0700

    Reposition the steps in creating user

--- server/src/routes/user/create.rs
+++ server/src/routes/user/create.rs
@@ -1,14 +1,15 @@
 use std::borrow::Cow::Borrowed;
 use serde::Deserialize;
 use axum::{ Extension, Json, extract::State, http::StatusCode };
-use mongodb::bson::{ self, doc };
+use mongodb::{ bson::{ self, doc }, error::WriteFailure };
 use validator::Validate;
 
 use crate::{
     base::{ self, response::ResponseModel },
-    store::users::UserDocument,
+    cache::verify::VerifyCacheQuery,
     middlewares::auth::{ AuthorizationInfo, AuthorizationStatus },
     routes::user::UserRoutesState,
+    store::users::UserDocument,
     workers::verify_email::VerifyEmailRequest,
 };
 use nanoid::nanoid;
@@ -22,12 +23,42 @@ pub struct CreatePayload {
     #[validate(length(max = 2048))]
     pub clientstile: String,
 }
+impl CreatePayload {
+    fn check(&self) -> Result<(), &str> {
+        if let Err(error) = self.validate() {
+            match error.errors().iter().next() {
+                Some((bad_field, _)) => {
+                    match bad_field {
+                        Borrowed("email") => {
+                            return Err("Invalid email.");
+                        }
+                        Borrowed("password") => {
+                            return Err("Password must be longer than 12 characters.");
+                        }
+                        Borrowed("clientstile") => {
+                            return Err("Something went wrong, refresh the page and try again.");
+                        }
+                        _ => {
+                            return Err("unknown");
+                        }
+                    }
+                }
+                None => {
+                    return Err("unknown");
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
 
 pub async fn handler(
     Extension(authorization_info): Extension<AuthorizationInfo>,
     State(state): State<UserRoutesState>,
     Json(payload): Json<CreatePayload>
 ) -> ResponseModel {
+    // Authentication status must not be `Authorized`.
     if let AuthorizationStatus::Authorized = authorization_info.status {
         return base::response::error(
             StatusCode::FORBIDDEN,
@@ -36,7 +67,7 @@ pub async fn handler(
         );
     }
 
-    // Vro I don't like too much processing yk.
+    // Accquire a semaphore permit.
     let permit = match state.semaphores.create.acquire().await {
         Ok(permit) => permit,
         Err(_) => {
@@ -45,49 +76,20 @@ pub async fn handler(
     };
 
     // Validate before processing.
-    if let Err(error) = payload.validate() {
-        match error.errors().iter().next() {
-            Some((bad_field, _)) => {
-                match bad_field {
-                    Borrowed("email") => {
-                        return base::response::error(
-                            StatusCode::BAD_REQUEST,
-                            "Invalid email.",
-                            None
-                        );
-                    }
-                    Borrowed("password") => {
-                        return base::response::error(
-                            StatusCode::BAD_REQUEST,
-                            "Password must be longer than 12 characters.",
-                            None
-                        );
-                    }
-                    Borrowed("clientstile") => {
-                        return base::response::error(
-                            StatusCode::BAD_REQUEST,
-                            "Invalid turnstile token length u cheeky lad UvU",
-                            None
-                        );
-                    }
-                    _ => {
-                        return base::response::internal_error(None);
-                    }
-                }
-            }
-            None => {
-                return base::response::internal_error(None);
-            }
+    match payload.check() {
+        Ok(_) => {} // valid, move on
+        Err(message) => {
+            return base::response::error(StatusCode::BAD_REQUEST, message, None);
         }
     }
 
-    // Check turnstile token.
+    // Verify clientstile token.
     match state.app.turnstile.verify(payload.clientstile).await {
         Ok(true) => {} // valid, move on
         Ok(false) => {
             return base::response::error(
                 StatusCode::BAD_REQUEST,
-                "Invalid turnstile token length u cheeky lad UvU",
+                "Something went wrong, try refresh the page and enter information again.",
                 None
             );
         }
@@ -96,7 +98,7 @@ pub async fn handler(
         }
     }
 
-    // Check if the email is already used.
+    // Check if email is used before hashing the password to avoid excessive computes.
     match state.app.store.users.get_one(doc! { "email": &payload.email }).await {
         Ok(None) => {} // valid, move on
         Ok(Some(_)) => {
@@ -112,6 +114,11 @@ pub async fn handler(
         }
     }
 
+    // Create a new user id.
+    let user_id = nanoid!(48);
+    let verify_code = nanoid!(64);
+
+    // Hash the password.
     let password_hash = match state.app.worker.hash_pass.send(payload.password).await {
         Ok(Some(hash)) => hash,
         // Err & Ok(None)
@@ -120,8 +127,38 @@ pub async fn handler(
         }
     };
 
-    let verify_code = nanoid!(64);
+    // Add the user to store (2nd pass).
+    let user = UserDocument {
+        id: user_id.clone(),
+        email: payload.email.clone(),
+        password_hash,
+        verify_requested: Some(bson::DateTime::now()),
+        created_at: None,
+        accept_refresh_after: None,
+    };
 
+    match state.app.store.users.add(user).await {
+        Ok(_) => {} // valid, move on
+        Err(error) => {
+            match *error.kind {
+                mongodb::error::ErrorKind::Write(WriteFailure::WriteError(ref write_error)) if
+                    write_error.code == 11000
+                => {
+                    return base::response::error(
+                        StatusCode::BAD_REQUEST,
+                        "An account with the same email already exists.",
+                        None
+                    );
+                }
+                _ => {
+                    eprintln!("Database error: {:?}", error.kind);
+                    return base::response::internal_error(None);
+                }
+            }
+        }
+    }
+
+    // Send a verification email to the user.
     if
         state.app.worker.verify_email
             .send_ignore(VerifyEmailRequest {
@@ -133,23 +170,22 @@ pub async fn handler(
         return base::response::internal_error(None);
     }
 
-    let user = UserDocument {
-        id: nanoid!(48),
-        email: payload.email,
-        password_hash,
-        verify_requested: Some(bson::DateTime::now()),
-        verify_code: Some(verify_code),
-        created_at: None,
-        accept_refresh_after: None,
-    };
-
-    drop(permit);
-
-    return match state.app.store.users.add(user).await {
-        Ok(_) => base::response::success(StatusCode::CREATED, None),
+    // Cache the verify code.
+    match
+        state.app.cache.verify.clone().add(VerifyCacheQuery {
+            user_id: user_id.clone(),
+            code: verify_code,
+        }).await
+    {
+        Ok(_) => {}
         Err(error) => {
             eprintln!("Database error: {:?}", error);
-            base::response::internal_error(None)
+            return base::response::internal_error(None);
         }
-    };
+    }
+
+    // Dropping semaphore permit, we done here.
+    drop(permit);
+
+    return base::response::success(StatusCode::CREATED, None);
 }
