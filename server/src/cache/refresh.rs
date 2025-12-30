06f2532f5d6b9d0aa366115df5031ae62f18985c
neursh <31346263+neursh@users.noreply.github.com>
2025-12-30 10:33:19 +0700

    Rename the query

--- server/src/cache/refresh.rs
+++ server/src/cache/refresh.rs
@@ -1,8 +1,8 @@
 use redis::{ AsyncCommands, aio::MultiplexedConnection };
 
-use crate::utils::session::REFRESH_MAX_AGE;
+use crate::consts::REFRESH_MAX_AGE;
 
-pub struct RefreshQuery {
+pub struct RefreshCacheQuery {
     pub user_id: String,
     pub created_at: i64,
 }
@@ -14,7 +14,7 @@ pub struct RefreshCache {
 }
 
 impl RefreshCache {
-    pub async fn add(&mut self, query: RefreshQuery) -> Result<(), redis::RedisError> {
+    pub async fn add(&mut self, query: RefreshCacheQuery) -> Result<(), redis::RedisError> {
         let key = format!("<{}>::jwt::refresh::{}", query.user_id, query.created_at);
         self.endpoint.set::<&str, bool, bool>(&key, true).await?;
         self.endpoint.expire::<&str, bool>(&key, REFRESH_MAX_AGE).await?;
@@ -23,7 +23,7 @@ impl RefreshCache {
 
     /// Check if the refresh token is valid, the entry then deletes, ensuring that
     /// refresh token can only be used once.
-    pub async fn permit(&mut self, query: RefreshQuery) -> Result<bool, redis::RedisError> {
+    pub async fn permit(&mut self, query: RefreshCacheQuery) -> Result<bool, redis::RedisError> {
         let key = format!("<{}>::jwt::refresh::{}", query.user_id, query.created_at);
         return Ok(self.endpoint.get_del::<&str, Option<bool>>(&key).await?.is_some());
     }
