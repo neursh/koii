fe3ec1e6e8599420ecde3d7d70dafccddae2ae97
neursh <31346263+neursh@users.noreply.github.com>
2026-04-15 15:25:52 +0700

    Build the url on create

--- src/utils/totp.rs
+++ src/utils/totp.rs
@@ -8,6 +8,7 @@ use thiserror::Error;
 #[derive(Serialize, Deserialize)]
 pub struct Totp {
     pub secret: Binary,
+    pub url: String,
 }
 
 #[derive(Error, Debug)]
@@ -18,27 +19,26 @@ pub enum TotpError {
 }
 
 impl Totp {
-    pub fn new() -> Self {
-        Totp {
-            secret: Binary {
-                subtype: mongodb::bson::spec::BinarySubtype::Generic,
-                bytes: nanoid::rngs::default(128),
-            },
-        }
-    }
+    pub fn new(user_id: String) -> Result<Self, TotpError> {
+        let secret = nanoid::rngs::default(128);
 
-    pub fn url(&self, user_id: String) -> Result<String, TotpError> {
         let totp = TOTP::new(
             totp_rs::Algorithm::SHA1,
             6,
             1,
             30,
-            self.secret.clone().bytes,
+            secret,
             Some("Koii".to_string()),
             user_id
         )?;
 
-        Ok(totp.get_url())
+        Ok(Totp {
+            secret: Binary {
+                subtype: mongodb::bson::spec::BinarySubtype::Generic,
+                bytes: nanoid::rngs::default(128),
+            },
+            url: totp.get_url(),
+        })
     }
 
     pub fn verify(&self, token: &str, email: String) -> Result<bool, TotpError> {
