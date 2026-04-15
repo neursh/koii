1c33d4dd61b6bb43747b7d2fa26839fbd818cf79
neursh <31346263+neursh@users.noreply.github.com>
2026-04-15 21:28:43 +0700

    Add authorize TOTP for other actions

--- src/routes/user/totp/authorize.rs
+++ src/routes/user/totp/authorize.rs
@@ -1,10 +1,52 @@
-use axum::{Extension, extract::State};
+use axum::{ Extension, Json, extract::State, http::StatusCode };
+use serde::Deserialize;
 
-use crate::{middlewares::auth::AuthorizationInfo, routes::user::UserRoutesState};
+use crate::{
+    base::{ self, response::ResponseModel },
+    middlewares::auth::{ AuthorizationInfo, AuthorizationStatus },
+    routes::user::UserRoutesState,
+};
+
+#[derive(Deserialize)]
+pub struct AuthorizePayload {
+    totp_code: String,
+}
 
 pub async fn handler(
     Extension(authorization_info): Extension<AuthorizationInfo>,
-    State(state): State<UserRoutesState>
-) {
-    
+    State(state): State<UserRoutesState>,
+    Json(payload): Json<AuthorizePayload>
+) -> ResponseModel {
+    match authorization_info.status {
+        AuthorizationStatus::Authorized => {} // Authorized, passing down.
+        _ => {
+            return base::response::error(StatusCode::UNAUTHORIZED, "Get out.", None);
+        }
+    }
+
+    let token = match authorization_info.token {
+        Some(token) => token,
+        None => {
+            return base::response::internal_error(None);
+        }
+    };
+
+    let totp = match state.app.db.user.document.get_totp(&token.user_id).await {
+        Ok(Some(totp)) => totp,
+        Ok(None) => {
+            return base::response::error(
+                StatusCode::NOT_FOUND,
+                "The account doesn't enable TOTP.",
+                None
+            );
+        }
+        Err(_) => {
+            return base::response::internal_error(None);
+        }
+    };
+
+    match totp.verify(&payload.totp_code) {
+        Ok(_) => todo!(),
+        Err(_) => todo!(),
+    }
 }
