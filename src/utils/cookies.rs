10ed680a36eb4e60ac537f888369ae315846ede5
neursh <31346263+neursh@users.noreply.github.com>
2026-05-03 23:06:41 +0700

    Add a remover cookie instruction

--- src/utils/cookies.rs
+++ src/utils/cookies.rs
@@ -2,11 +2,23 @@ use std::time::Duration;
 
 use cookie_rs::{ Cookie, cookie::SameSite };
 
-pub fn construct(name: &str, value: String, max_age: Duration) -> String {
+pub fn construct(name: &str, value: String, path: &str, max_age: Duration) -> String {
     Cookie::builder(name, value)
         .domain(".koii.space")
-        .path("/")
+        .path(path)
         .max_age(max_age)
+        .same_site(SameSite::Lax)
+        .http_only(true)
+        .secure(true)
+        .build()
+        .to_string()
+}
+
+pub fn remove(name: &str, path: &str) -> String {
+    Cookie::builder(name, "X")
+        .domain(".koii.space")
+        .path(path)
+        .max_age(Duration::from_secs(0))
         .same_site(SameSite::Lax)
         .http_only(true)
         .secure(true)
