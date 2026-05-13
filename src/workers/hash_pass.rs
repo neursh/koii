1af84b9f2b0d93aaad56add312a557614bbd7e13
neursh <31346263+neursh@users.noreply.github.com>
2026-05-13 12:28:38 +0700

    Apply argon2id consts config

--- src/workers/hash_pass.rs
+++ src/workers/hash_pass.rs
@@ -1,7 +1,20 @@
 use std::thread;
 
-use argon2::{ Argon2, password_hash::{ PasswordHasher, SaltString, rand_core::OsRng } };
+use argon2::{
+    Algorithm,
+    Argon2,
+    Params,
+    Version,
+    password_hash::{ PasswordHasher, SaltString, rand_core::OsRng },
+};
 use tokio::sync::{ oneshot };
+
+use crate::consts::{
+    ARGON2_MEMORY_COST,
+    ARGON2_OUTPUT_LENGTH,
+    ARGON2_PARALLELISM_COST,
+    ARGON2_TIME_COST,
+};
 
 pub fn launch(
     rx: kanal::AsyncReceiver<(String, Option<oneshot::Sender<Option<String>>>)>,
@@ -14,7 +27,16 @@ pub fn launch(
 }
 
 fn worker(rx: kanal::Receiver<(String, Option<oneshot::Sender<Option<String>>>)>) {
-    let argon2 = Argon2::default();
+    let argon2 = Argon2::new(
+        Algorithm::Argon2id,
+        Version::V0x13,
+        Params::new(
+            ARGON2_MEMORY_COST,
+            ARGON2_TIME_COST,
+            ARGON2_PARALLELISM_COST,
+            Some(ARGON2_OUTPUT_LENGTH)
+        ).unwrap()
+    );
     while let Ok((password, Some(sender))) = rx.recv() {
         let salt = SaltString::generate(&mut OsRng);
         match argon2.hash_password(password.as_bytes(), &salt) {
