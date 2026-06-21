f725eef62329149e7507a599da4460b308292dc4
neursh <31346263+neursh@users.noreply.github.com>
2026-06-21 09:55:30 +0700

    Add DB transaction to add TOTP entry and update account document

--- src/database/totp/store.rs
+++ src/database/totp/store.rs
@@ -1,22 +1,32 @@
-use mongodb::{ Collection, IndexModel, bson, error::WriteFailure, options::IndexOptions };
+use mongodb::{
+    ClientSession,
+    Collection,
+    IndexModel,
+    bson,
+    error::WriteFailure,
+    options::IndexOptions,
+};
 use serde::{ Deserialize, Serialize };
 
 use crate::utils::totp::Totp;
 
-#[derive(Deserialize, Serialize)]
+#[derive(Clone, Deserialize, Serialize)]
 pub struct TotpStoreDocument {
     /// Unique ID to the account.
     pub account_id: String,
     pub totp: Totp,
 }
 
+#[derive(Clone)]
 pub struct TotpStoreOperations {
     collection: Collection<TotpStoreDocument>,
+    mongo_client: mongodb::Client,
 }
 
 impl TotpStoreOperations {
     pub async fn new(
-        collection: Collection<TotpStoreDocument>
+        collection: Collection<TotpStoreDocument>,
+        mongo_client: mongodb::Client
     ) -> Result<Self, mongodb::error::Error> {
         collection.create_index(
             IndexModel::builder()
@@ -25,27 +35,49 @@ impl TotpStoreOperations {
                 .build()
         ).await?;
 
-        Ok(TotpStoreOperations { collection })
+        Ok(TotpStoreOperations { collection, mongo_client })
     }
 
-    pub async fn add(&self, document: &TotpStoreDocument) -> Result<bool, mongodb::error::Error> {
-        match self.collection.insert_one(document).await {
-            Ok(_) => {}
+    /// This method performs a transaction between `account` and `totp` collection, hence
+    /// the need for the additional of mongodb client and account collection.
+    pub async fn add(&self, document: TotpStoreDocument) -> Result<bool, mongodb::error::Error> {
+        let mut session = self.mongo_client.start_session().await?;
+
+        let result = session.start_transaction().and_run2(async move |session: &mut ClientSession| {
+            let collection = session
+                .client()
+                .database("koii")
+                .collection::<TotpStoreDocument>("totp");
+            let account_collection = session
+                .client()
+                .database("koii")
+                .collection::<TotpStoreDocument>("account");
+
+            collection.insert_one(&document).session(&mut *session).await?;
+            let result = account_collection
+                .update_one(
+                    bson::doc! { "account_id": &document.account_id },
+                    bson::doc! {
+                        "$set": {
+                            "mfa_status.totp": true
+                        }
+                    }
+                )
+                .session(session).await?;
+            Ok(result.modified_count == 1)
+        }).await;
+
+        return match result {
+            Ok(modified) => Ok(modified),
             Err(error) => {
                 match *error.kind {
                     mongodb::error::ErrorKind::Write(WriteFailure::WriteError(ref write_error)) if
                         write_error.code == 11000
-                    => {
-                        return Ok(false);
-                    }
-                    _ => {
-                        return Err(error);
-                    }
+                    => Ok(false),
+                    _ => Err(error),
                 }
             }
-        }
-
-        Ok(true)
+        };
     }
 
     pub async fn get_from_account(
@@ -62,9 +94,33 @@ impl TotpStoreOperations {
         }
     }
 
-    pub async fn delete(&self, account_id: &str) -> Result<bool, mongodb::error::Error> {
-        let result = self.collection.delete_one(bson::doc! { "account_id": account_id }).await?;
+    pub async fn delete(&self, account_id: String) -> Result<bool, mongodb::error::Error> {
+        let mut session = self.mongo_client.start_session().await?;
 
-        Ok(result.deleted_count == 1)
+        session.start_transaction().and_run2(async move |session: &mut ClientSession| {
+            let account_id = account_id.clone();
+
+            let database = session.client().database("koii");
+
+            let collection = database.collection::<TotpStoreDocument>("totp");
+            let account_collection = database.collection::<TotpStoreDocument>("account");
+
+            collection
+                .delete_one(bson::doc! { "account_id": &account_id })
+                .session(&mut *session).await?;
+            account_collection
+                .update_one(
+                    bson::doc! { "account_id": &account_id },
+                    bson::doc! {
+                        "$set": {
+                            "mfa_status.totp": true
+                        }
+                    }
+                )
+                .session(session).await?;
+            Ok(())
+        }).await?;
+
+        Ok(true)
     }
 }
